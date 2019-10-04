a750d64edc0d365a7d7e17b0192e67008a3b42e6
Martin Heidegger <martin.heidegger@gmail.com>
2019-10-04 21:49:39 +0900

    Exporting crypto as "crypto" in order to prevent overriding of the setup command.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-import { setup } from '@consento/crypto'
 import { IAPI, IAPIOptions } from './types'
 import { Notifications } from './notifications'
 
-export * from '@consento/crypto'
+import * as crypto from '@consento/crypto'
+
+export {
+  crypto
+}
 export * from './notifications/types'
 export * from './types'
 
 export function api ({ cryptoCore, notificationTransport }: IAPIOptions): IAPI {
-  const crypto = setup(cryptoCore)
   return {
     notifications: new Notifications({
       transport: notificationTransport
     }),
-    crypto
+    crypto: crypto.setup(cryptoCore)
   }
 }
