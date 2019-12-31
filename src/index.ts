0a9c5adde0f2aa39c7e5aadf1f5be8916a23fad8
Martin Heidegger <martin.heidegger@gmail.com>
2019-12-31 19:45:48 +0900

    Removed reference to cancelable as it is uppercase in the filesystem and would be broken in bundler

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Notifications } from './notifications'
 import { setup as cryptoSetup } from '@consento/crypto'
 
 export * from './types'
-export * from '@consento/crypto/util/cancelable'
+export * from '@consento/crypto/types'
 export { isSuccess as isSuccessNotification, isError as isErrorNotification } from './notifications'
 
 export function setup ({ cryptoCore, notificationTransport }: IAPIOptions): IAPI {
