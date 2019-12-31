069dfd46c3d91cf0b9bca703ddd80817c310feef
Martin Heidegger <martin.heidegger@gmail.com>
2019-12-31 21:18:33 +0900

    Exporting more than just the types

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Notifications } from './notifications'
 import { setup as cryptoSetup } from '@consento/crypto'
 
 export * from './types'
-export * from '@consento/crypto/types'
+export * from '@consento/crypto'
 export { isSuccess as isSuccessNotification, isError as isErrorNotification } from './notifications'
 
 export function setup ({ cryptoCore, notificationTransport }: IAPIOptions): IAPI {
